3b25a2db9e8ebe23d18494b854083cf3c6bb16bb
Dmitriy Gerasimov <dmitriy_gerasimov_02@mail.ru>
2024-01-14 23:18:49 +0300

    fix-lab6-3

--- Scripts/script_http.js
+++ Scripts/script_http.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener('DOMContentLoaded', async function () {
   let flag = false;
   const limit = 4;
-  const refreshButton = document.getElementById("refresh");
-  refreshButton.addEventListener("click", function () {
+  const refreshButton = document.getElementById('refresh');
+  refreshButton.addEventListener('click', function () {
     flag = !flag;
     updateRequest(flag, limit);
   });
@@ -10,41 +10,42 @@ document.addEventListener("DOMContentLoaded", async function () {
 });
 
 async function updateRequest(firstCall, limit) {
-  const preloader = document.getElementById("preloader");
-  const errorPlaceholder = document.createElement("div");
-  let url = "https://jsonplaceholder.typicode.com/comments";
+  const preloader = document.getElementById('preloader');
+  const errorPlaceholder = document.createElement('div');
+  let url = 'https://jsonplaceholder.typicode.com/comments';
   if (firstCall) {
+    // const limit = 7;
     const randomStart = Math.floor(Math.random() * limit);
-    url += "?_start=${randomStart}&_limit=${limit}";
+    url += '?_start=${randomStart}&_limit=${limit}';
   } else {
-    url += "?_start=${limit}&_limit=${limit}";
+    url += '?_start=${limit}&_limit=${limit}';
   }
   try {
-    preloader.style.display = "block";
+    preloader.style.display = 'block';
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error("ERROR");
+      throw new Error('ERROR');
     }
     const postsList = await response.json();
-    console.log("Данные:", postsList);
+    console.log('Данные:', postsList);
     renderPosts(postsList);
-    preloader.style.display = "none";
+    preloader.style.display = 'none';
   } catch (error) {
-    console.error("Error:", error);
-    preloader.style.display = "none";
-    errorPlaceholder.textContent = "⚠️ Что-то пошло не так";
-    document.getElementById("postsList").appendChild(errorPlaceholder);
+    console.error('Error:', error);
+    preloader.style.display = 'none';
+    errorPlaceholder.textContent = '⚠️ Что-то пошло не так';
+    document.getElementById('postsList').appendChild(errorPlaceholder);
   }
 }
 
 function renderPosts(postsList) {
-  const postsListElement = document.getElementById("postsList");
-  postsListElement.innerHTML = "";
-  const template = document.getElementById("postTemplate");
+  const postsListElement = document.getElementById('postsList');
+  postsListElement.innerHTML = '';
+  const template = document.getElementById('postTemplate');
   postsList.forEach((post) => {
     const postElement = document.importNode(template.content, true);
-    postElement.querySelector(".email").textContent = "Email: " + post.email;
-    postElement.querySelector(".body").textContent = "Body: " + post.body;
+    postElement.querySelector('.email').textContent = 'Email: ' + post.email;
+    postElement.querySelector('.body').textContent = 'Body: ' + post.body;
     postsListElement.appendChild(postElement);
   });
 }
